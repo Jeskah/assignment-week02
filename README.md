17bf7c49803648aeb70c57f0193cd0e1983b48c0
Jessica <jessica.antoinette@outlook.com>
2026-01-16 16:26:25 +0000

    updated

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # assignment-week02
+
+This week, to say the least has been a challenge and I don't think I've ever used my brain to try and link things together with computer logic, moving through times of understanding something with "Ah-ha" moments to totally losing a thread and having to restudy sections to patch up knowledge that got a little lost. 
+
+Mostly victories but at this point it's a matter of reading and writing the code consistenly and stregthening my understanding of JavaScript in particular by repeating it and familiarising myself with how the language operates which I can see improvements in already and making sure I study lots in my own time to catch up on things I feel I could fall behind with as I'm also a little nervous about asking for help and might spend too much time trying to figure things out on my own instead but I feel I learn better that way as sometimes information can overwhelm me
+
+I'm currently also designing some cards in illustrator to help me make visual links to the operations of Javascript and that is helping to solidify the basics. So far it's still very enjoyable and while I'm studying in my own time to catch up on things that might not sink in the first time the challenge has been fun and rewarding and not yet hit a stage where I think I can't do something despite behing somewhat out of my depth. 
+
+Rinse and repeat!
--- app.js
+++ app.js
@@ -1,41 +1,74 @@
 console.log(`Say Cheese...`)
 
+// selecting thumbnail container from element in HTML
 const thumbnailsContainer = document.querySelector(`.thumbnails`)
 const bigDisplay = document.getElementById(`bigDisplay`)
 
+
+// add arrays - collection of objects - this case imgages
 const images = [
     {src: 'assets/1.jpg', 
-    alt: `image1`},
+    alt: `arcade`},
 
     {src: 'assets/2.jpg', 
-    alt: `image2`},
+    alt: `inky`},
 
     {src: 'assets/3.jpg', 
-    alt: `image 3`},
+    alt: `inky1`},
 
     {src: 'assets/4.jpg', 
-    alt: `image 4`},
+    alt: `light`},
 
     {src: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fnamingcat.com%2Fwp-content%2Fuploads%2F2024%2F04%2F80s-Cat-Names.jpg&f=1&nofb=1&ipt=a6bd13f73e8a2ecf2bbefdbbcc4551ed35bb58c8cfec24a0eaea038edef25c06',
-    alt: `image 5`},
+    alt: `pyskitty`},
 
     {src: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpaperaccess.com%2Ffull%2F2989122.jpg&f=1&nofb=1&ipt=ed7cee31dc7cf822a237ecfd6f0b2f332da42eaa7519d8fb12796ce2891cea93',
-    alt: `image 6`},
+    alt: `psykitty1`},
 
-]
+] //sources/data
 
 
 
 function createThumbnails() {
-    images.forEach(function(image) {
+    
+        images.forEach(function(image) {  
+        //for, forEach Loops, needs 'callback function' as its "argument"
+        //image is the perameter, they are the images
 
         const imageElement = document.createElement('img')
+        // creating the imageElement with the 'img' tag
 
+        console.log(image)
+
+        // setting img tag up to link the content of objects 
         imageElement.src = image.src
         imageElement.alt = image.alt
 
+        imageElement.addEventListener('click', function () {
+            createBigImage(image)
+        })
+
+        //create tag - set its content - then APPEND to DOM
         thumbnailsContainer.append(imageElement)
 })
 }
 
-createThumbnails()
+
+createThumbnails() //call the funtion / envoke it ot it will not display
+
+
+function createBigImage(imageData) {
+
+    bigDisplay.innerHTML = ''
+
+    const bigImage = document.createElement('img')
+
+    bigImage.src = imageData.src
+    bigImage.alt = imageData.alt
+
+
+    bigDisplay.append(bigImage)
+
+}
+
+createBigImage(images[0])
